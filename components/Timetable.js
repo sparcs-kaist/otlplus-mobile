6b3a8c1bc01128ab3a3f099c7fe21bd3bf9cbff6
Bhang Hyoung Jo <bhanghj3094@gmail.com>
2019-05-28 01:27:14 +0900

    basic content div

--- components/Timetable.js
+++ components/Timetable.js
@@ -6,8 +6,29 @@ import {
 
 const Timetable = () => (
   <View>
-    {/*  Title section */}
-    <Text>This is the Timetable</Text>
+    <View>
+      <Text>This is the Timetable</Text>
+      {/* change the css(timetable height) and content info(show or not) when this.fullTimetable is false */}
+      {/* 월 화 수 목 금 */}
+      {/* 왼쪽 시간 bar */}
+      {/* 월 div */}
+      {/* 화 div */}
+      {/* 수 div */}
+      {/* 목 div */}
+      {/* 금 div */}
+    </View>
+    <View>
+      <View>
+        {/* 기필, 전필, 인문,
+            기선, 전선, 기타 */}
+      </View>
+      <View>
+        {/* xx학점, xAU */}
+      </View>
+      <View>
+        {/* 성적, 널널, 강의 */}
+      </View>
+    </View>
   </View>
 );
 
